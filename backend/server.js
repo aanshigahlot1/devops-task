2f92f5d47b238f30f8048c5b825ad5fdb1ec314f
aanshigahlot1 <aanshigahlot026@gmail.com>
2026-02-24 02:44:57 +0530

    Added CI/CD pipeline

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,42 @@
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
-// parse requests of content-type - application/json
-app.use(express.json());
+app.use(cors());
 
-// parse requests of content-type - application/x-www-form-urlencoded
+// ===== BODY PARSER =====
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// ===== DATABASE =====
 const db = require("./app/models");
+
+console.log("Mongo URL:", db.url);
+
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to the database!");
   })
-  .catch(err => {
+  .catch((err) => {
     console.log("Cannot connect to the database!", err);
-    process.exit();
   });
 
-// simple route
+// ===== TEST ROUTE =====
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Test application." });
 });
 
+// ===== ROUTES =====
 require("./app/routes/turorial.routes")(app);
 
-// set port, listen for requests
+// ===== SERVER START =====
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
